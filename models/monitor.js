a92d42250254a8ce53c3b9ba2bdcc6612e58a045
vjudge <gradonday@gmail.com>
2021-10-06 20:32:36 +0800

    add models

---
+++ models/monitor.js
@@ -0,0 +1,12 @@
+const _ = require('lodash')
+
+const debug = require('debug')('models:monitor')
+
+module.exports = {
+    fetchMonitor
+}
+
+async function fetchMonitor () {
+  let result = 'OK'
+  return result
+}
